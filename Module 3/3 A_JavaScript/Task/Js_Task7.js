c0434f3f324e3e6906d59082d9d69617f10539e6
iammanny <mannyaikpe@gmail.com>
2024-07-27 17:09:05 +0100

    Update JavaScript Task 7

---
+++ Module 3/3 A_JavaScript/Task/Js_Task7.js
@@ -0,0 +1,12 @@
+const side1 = 1, side2 = 2, side3 = 2;
+
+const day = 'Thursday'
+if (side1 === side2 === side3){
+    console.log("This is an Equilateral triangle")
+}
+else if (side1 === side2 || side1 === side3 || side2 === side3){
+    console.log("This is an Isosceles triangle ")
+}
+else{
+    console.log("This is a Scalene triangle")
+}
