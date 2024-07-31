d76e2b8d1aec8620c2452a9706161cacef263fec
iammanny <mannyaikpe@gmail.com>
2024-07-31 19:43:24 +0100

    Update JavaScript Task 9

---
+++ Module 3/3 A_JavaScript/Task/Js_Task9.js
@@ -0,0 +1,17 @@
+// Task 9: Area Of a Rectangle
+
+
+// Write JavaScript function, calculateAreaOfRectangle. The function should
+
+// Take two parameters, length and width. Multiply the length and width
+
+// Log the product to the console
+
+function calculateAreaOfRectangle(length, width) {
+    const multiply = length * width;
+
+    const area = length * width;
+    console.log ("The Area of the Rectangle is " + area);
+
+}
+calculateAreaOfRectangle(8, 16);
