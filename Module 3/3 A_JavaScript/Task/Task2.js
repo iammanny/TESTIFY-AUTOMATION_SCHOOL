a560adfd2de53fb82dea836f6f0837c87751c815
iammanny <mannyaikpe@gmail.com>
2024-07-31 11:39:39 +0100

    Update Task Task2 & Task4

---
+++ Module 3/3 A_JavaScript/Task/Task2.js
@@ -0,0 +1,10 @@
+// Js fun facts: Share some fun facts you know about about the 
+// JavaScript progrgramming language. Follow the link below to get started.
+
+
+
+// HOW TO SUMIT
+
+// NOTE: 
+// Push your code to Github Repository and ensure your Jenkins job run.
+
