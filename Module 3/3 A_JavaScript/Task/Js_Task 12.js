480be9389fb1a73cab0e244c9ecce61768fe380f
iammanny <mannyaikpe@gmail.com>
2024-08-01 10:32:11 +0100

    Update JavaScript Task 12

---
+++ Module 3/3 A_JavaScript/Task/Js_Task 12.js
@@ -0,0 +1,19 @@
+// Task: Pick your Best Friend
+// Elements of an array are index based. 
+
+// From your array of best created in the previous task, 
+// choose your best friend based on their index in the array. 
+
+// Steps: 
+
+// On the next line below your array of friends, Declare a variable, bestFriend 
+// and assign your best friend from the array of friends. (Use bracket notation)
+
+// Log bestFriend to the console to ensure you chose the right friend.
+
+
+const friendsArray = ['Victory', 'Allen', 'Ada', 'Ayo', 'Benis']
+
+const bestFriend = friendsArray[1]
+
+console.log(bestFriend)
