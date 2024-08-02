f052d398c66076d4b7dfe6bf4563e1bb20c337fc
iammanny <mannyaikpe@gmail.com>
2024-08-02 09:56:20 +0100

    Update JS Coding Challenge Task 8

---
+++ Module 3/3 A_JavaScript/JavaScript Code Challenegs/Task8.js
@@ -0,0 +1,10 @@
+//8. Return a Boolean if a number is divisible by 10
+
+const booleanNum = 20
+
+if (booleanNum % 10 === 0) {
+    console.log("true"); // return true
+}
+else{
+    console.log("false!");  // return false
+}
